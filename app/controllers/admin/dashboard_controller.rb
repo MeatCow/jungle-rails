d5a383d60bede25d21439cb453e628b144137754
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-01 16:40:38 -0400

    Feature: Implement HTTP Basic auth for admin pages

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class DashboardController < ApplicationController
+    http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
     def show; end
   end
 end
