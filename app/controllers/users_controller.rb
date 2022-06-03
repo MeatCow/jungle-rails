48e66773f376491e2f731ff50155df442ec2e6d5
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-03 16:31:23 -0400

    Implement remaining functionality of login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      session[:user_id] = user.id
-      redirect_to :index, notice: 'User created!'
+      session[:user_id] = @user.id
+      redirect_to '/'
     else
       render :new
     end
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
