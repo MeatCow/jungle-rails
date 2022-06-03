73d2916ba6c207d8ad06227959007bac2b1ec2b1
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-03 03:45:58 -0400

    Feature: Create user model and default view routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,10 @@ Rails.application.routes.draw do
 
   get 'about' => 'about#show'
 
+  # These routes are used for signing up.
+  get '/signup', to: 'users#new'
+  get '/users', to: 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
