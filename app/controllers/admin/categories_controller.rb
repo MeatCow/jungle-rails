39dd9006531aebf90094b495e01c2b83666ff020
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-03 09:41:47 -0400

    Fix: Add auth to admin categories page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class CategoriesController < ApplicationController
+    http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
     def index
       @categories = Category.order(id: :asc).all
     end
