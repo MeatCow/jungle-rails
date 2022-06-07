2cb43862d426d284569db0b7dff6813cd26795be
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-06 22:44:16 -0400

    Doc: Update tests to verify full_message content

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,31 +9,32 @@ describe Product, type: :model do
     it 'should save if all required fields are present' do
       plant = @category.products.new({ name: 'grass', price: 5.00, quantity: 5 })
       plant.validate
-      expect(plant.errors).to be_empty
+      expect(plant.errors.full_messages).to be_empty
     end
 
     it 'should cause an error if name is not present' do
       plant = @category.products.new({ price: 5.00, quantity: 5 })
       plant.validate
-      expect(plant.errors[:name]).to contain_exactly "can't be blank"
+      expect(plant.errors.full_messages).to contain_exactly "Name can't be blank"
     end
 
     it 'should cause an error if price is not present' do
       plant = @category.products.new({ name: 'grass', quantity: 5 })
       plant.validate
-      expect(plant.errors[:price]).to contain_exactly "can't be blank", 'is not a number'
+      expect(plant.errors.full_messages).to contain_exactly "Price can't be blank", 'Price is not a number',
+                                                            'Price cents is not a number'
     end
 
     it 'should cause an error if quantity is not present' do
       plant = @category.products.new({ name: 'grass', price: 5.00 })
       plant.validate
-      expect(plant.errors[:quantity]).to contain_exactly "can't be blank"
+      expect(plant.errors.full_messages).to contain_exactly "Quantity can't be blank"
     end
 
     it 'should cause an error if category is not present' do
       plant = Product.new({ name: 'grass', price: 5.00, quantity: 5 })
       plant.validate
-      expect(plant.errors[:category]).to contain_exactly "can't be blank", 'must exist'
+      expect(plant.errors.full_messages).to contain_exactly "Category can't be blank", 'Category must exist'
     end
   end
 end
