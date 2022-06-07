024e0a81ed3ed617442ff6c226521ef711669250
Matthieu Pauze <matt.pauze@gmail.com>
2022-06-07 00:14:54 -0400

    Doc: Update product spec to use include instead of contain_exactly

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,26 +17,25 @@ describe Product, type: :model do
     it 'should cause an error if name is not present' do
       plant = @category.products.new({ price: 5.00, quantity: 5 })
       plant.validate
-      expect(plant.errors.full_messages).to contain_exactly "Name can't be blank"
+      expect(plant.errors.full_messages).to include "Name can't be blank"
     end
 
     it 'should cause an error if price is not present' do
       plant = @category.products.new({ name: 'grass', quantity: 5 })
       plant.validate
-      expect(plant.errors.full_messages).to contain_exactly "Price can't be blank", 'Price is not a number',
-                                                            'Price cents is not a number'
+      expect(plant.errors.full_messages).to include "Price can't be blank"
     end
 
     it 'should cause an error if quantity is not present' do
       plant = @category.products.new({ name: 'grass', price: 5.00 })
       plant.validate
-      expect(plant.errors.full_messages).to contain_exactly "Quantity can't be blank"
+      expect(plant.errors.full_messages).to include "Quantity can't be blank"
     end
 
     it 'should cause an error if category is not present' do
       plant = Product.new({ name: 'grass', price: 5.00, quantity: 5 })
       plant.validate
-      expect(plant.errors.full_messages).to contain_exactly "Category can't be blank", 'Category must exist'
+      expect(plant.errors.full_messages).to include "Category can't be blank"
     end
   end
 end
